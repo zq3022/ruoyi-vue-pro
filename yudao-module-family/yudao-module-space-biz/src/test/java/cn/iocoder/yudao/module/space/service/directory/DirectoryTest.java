9635b104bfe69d164eb8ded8e8e2c030a1b41d15
zq3022 <zq3022@126.com>
2023-12-05 21:26:54 +0800

    space:图片的metadata-extractor测试

--- yudao-module-family/yudao-module-space-biz/src/test/java/cn/iocoder/yudao/module/space/service/directory/DirectoryTest.java
+++ yudao-module-family/yudao-module-space-biz/src/test/java/cn/iocoder/yudao/module/space/service/directory/DirectoryTest.java
@@ -1,11 +1,15 @@
 package cn.iocoder.yudao.module.space.service.directory;
 
-import cn.iocoder.yudao.framework.test.core.ut.BaseDbUnitTest;
 import cn.iocoder.yudao.framework.test.core.ut.BaseMockitoUnitTest;
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -16,6 +20,7 @@ import java.nio.file.attribute.BasicFileAttributes;
  *
  * @author 芋道源码
  */
+@Slf4j
 public class DirectoryTest extends BaseMockitoUnitTest {
     @SneakyThrows
     @Test
@@ -41,6 +46,30 @@ public class DirectoryTest extends BaseMockitoUnitTest {
             }
 
         });
+    }
+
+
+    @SneakyThrows
+    @Test
+    public void testDirectoryFile() {
+        String filePath = "/Volumes/d_qzh/photo/底湖亲人/IMG_20180911_102212.JPG";
+        File file = new File(filePath);
+        Metadata metadata = ImageMetadataReader.readMetadata(file);
+        if (metadata == null) {
+            log.info("cannot find metadata");
+            return;
+        }
+
+        for (Directory directory : metadata.getDirectories()) {
+            if (directory == null) {
+                continue;
+            }
+            log.info(".........{}",directory.getName());
+
+            for (Tag tag : directory.getTags()) {
+                log.info("tagType:{}, tagName:{}, description: {}",tag.getTagType(), tag.getTagName(), tag.getDescription());
+            }
+        }
     }
 
 }
